a4677a3c6ca79ea040e2c69a98d184401cd09d90
Soltan Abbasov <soltanaa@code.edu.az>
2019-07-07 22:52:26 +0400

    some parts are not ready

--- MyLibrary/Program.cs
+++ MyLibrary/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyLibrary.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace MyLibrary
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
